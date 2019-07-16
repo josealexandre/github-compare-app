f56ef3d9c3fac088d2dbc144da3872c699acd9bb
Alexandre Junior <jasj_jr@hotmail.com>
2019-07-16 11:05:00 -0300

    Getting repositories from API and validating prop types

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import PropTypes from 'prop-types';
+
 import { Container, Repository } from './styles';
 
 const CompareList = ({ repositories }) => (
   <Container>
     {repositories.map(repository => (
-      <Repository>
+      <Repository key={repository.id}>
         <header>
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <strong>{repository.name}</strong>
@@ -30,5 +32,22 @@ const CompareList = ({ repositories }) => (
     ))}
   </Container>
 );
+
+CompareList.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        avatar_url: PropTypes.string,
+        login: PropTypes.string,
+      }),
+      stargazers_count: PropTypes.number,
+      forks_count: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default CompareList;
